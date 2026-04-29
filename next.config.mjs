00d9afbc71f03192ed6e06df9dd89d9ed485e6ba
Suhail1608 <suhailbasha1608@gmail.com>
2026-04-29 19:10:25 +0530

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
